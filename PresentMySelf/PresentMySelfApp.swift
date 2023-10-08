69671be2e4edacbb76047c7c830f2b591442b4f5
deemh <de.alboqmi@gmail.com>
2023-10-08 10:19:08 +0300

    Initial Commit

---
+++ PresentMySelf/PresentMySelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMySelfApp.swift
+//  PresentMySelf
+//
+//  Created by Deemh Albaqami on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PresentMySelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
